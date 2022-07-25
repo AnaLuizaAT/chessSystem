12d511734ee128f2b0e66c4ecf22faf7e9c66b75
AnaLuizaAT <aluiza.araujot@gmail.com>
2022-07-25 15:20:41 -0400

    First Commit

---
+++ src/boardgame/Position.java
@@ -0,0 +1,18 @@
+package src.boardgame;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Position {
+    private int row;
+    private int column;
+
+    @Override
+    public String toString() {
+        return row + "," + column;
+    }
+}
